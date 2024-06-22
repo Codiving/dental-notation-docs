411d1f18e570450767cfb6969d97033defde7e95
Codiving <codivingmoon@gmail.com>
2024-06-22 21:45:55 +0900

    feat: Add Installing

---
+++ src/component/Installing.tsx
@@ -0,0 +1,68 @@
+const Installing = () => {
+  return (
+    <div>
+      <h3 className="sectionTitle mt80">Installing</h3>
+      <h6 style={{ fontWeight: "bold", fontSize: 18, marginBottom: 20 }}>
+        Package manager
+      </h6>
+      <div style={{ display: "flex", flexDirection: "column", gap: 20 }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 14
+          }}
+        >
+          <p>Using npm:</p>
+          <p
+            style={{
+              backgroundColor: "#f6f8fa",
+              padding: 16
+              // maxWidth: 641
+            }}
+          >
+            npm install dental-notation
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 14
+          }}
+        >
+          <p>Using npm:</p>
+          <p
+            style={{
+              backgroundColor: "#f6f8fa",
+              padding: 16
+              // maxWidth: 641
+            }}
+          >
+            npm install dental-notation
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 14
+          }}
+        >
+          <p>Using npm:</p>
+          <p
+            style={{
+              backgroundColor: "#f6f8fa",
+              padding: 16
+              // maxWidth: 641
+            }}
+          >
+            npm install dental-notation
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Installing;
