cc1294c459324386f737a91edc754bb6ff36b7b2
Codiving <codivingmoon@gmail.com>
2024-06-22 21:47:16 +0900

    feat: Add total pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import DentalNotationTest from "@/component/DentalNotationTest";
+import DentalNotationTypes from "@/component/DentalNotationTypes";
+import Installing from "@/component/Installing";
+import Introduction from "@/component/Introduction";
+
 export default function Home() {
-  return null;
+  return (
+    <>
+      <header>
+        <div className="headerWrap">
+          <h1>Dental Notation</h1>
+        </div>
+      </header>
+
+      <main>
+        {/* Introduction to Dental Notation Packages */}
+        <Introduction />
+        {/* Installing */}
+        <Installing />
+        {/* Dental Notation Types */}
+        <DentalNotationTypes />
+        {/* Try it out yourself */}
+        <DentalNotationTest />
+      </main>
+    </>
+  );
 }
