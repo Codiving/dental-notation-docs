428d8afaa00e749881d3a1ef26deb750057d12bb
Codiving <codivingmoon@gmail.com>
2024-06-26 19:14:43 +0900

    feat: Add DentalNotationTest component for user test

--- src/component/DentalNotationTest.tsx
+++ src/component/DentalNotationTest.tsx
@@ -5,12 +5,30 @@ import {
   FDI_PRIMARY_TEETH_LOWER_RIGHT,
   FDI_PRIMARY_TEETH_UPPER_LEFT,
   FDI_PRIMARY_TEETH_UPPER_RIGHT,
+  FDI_TEETH_LOWER_LEFT_WITH_TMP,
+  FDI_TEETH_LOWER_RIGHT_WITH_TMP,
+  FDI_TEETH_UPPER_LEFT_WITH_TMP,
+  FDI_TEETH_UPPER_RIGHT_WITH_TMP,
+  FdiTeethLowerLeftWithTmp,
+  FdiTeethLowerRightWithTmp,
+  FdiTeethUpperLeftWithTmp,
+  FdiTeethUpperRightWithTmp,
   FdiTeethWithTmp,
-  convertTeeth,
-  convertTeethWithTmp
+  switchTeethWithTmp
 } from "dental-notation";
 import { useState } from "react";
 import NotationDescription from "./NotationDescription";
+
+const updateTeeth = (
+  teeth: FdiTeethWithTmp[],
+  reference: FdiTeethWithTmp[],
+  switched: FdiTeethWithTmp[]
+): FdiTeethWithTmp[] => {
+  return teeth.map(tooth => {
+    const index = reference.findIndex(refTooth => refTooth === tooth);
+    return index !== -1 ? switched[index] : tooth;
+  }) as FdiTeethWithTmp[];
+};
 
 const DentalNotationTest = () => {
   const [urTeeth, setUrTeeth] = useState<FdiTeethWithTmp[]>([
@@ -27,8 +45,47 @@ const DentalNotationTest = () => {
   ]);
 
   const [selectedTeeth, setSelectedTeeth] = useState<FdiTeethWithTmp[]>([]);
-  const onChangeSelectedTeeth = (teeth: FdiTeethWithTmp[]) =>
+
+  const onChangeSelectedTeeth = (
+    teeth: FdiTeethWithTmp[],
+    isConvert: boolean = false
+  ) => {
     setSelectedTeeth(teeth);
+
+    if (!isConvert) return;
+
+    const originTeeth = switchTeethWithTmp(teeth);
+    const ur = originTeeth.filter(tooth =>
+      FDI_TEETH_UPPER_RIGHT_WITH_TMP.includes(
+        tooth as FdiTeethUpperRightWithTmp
+      )
+    );
+    const ul = originTeeth.filter(tooth =>
+      FDI_TEETH_UPPER_LEFT_WITH_TMP.includes(tooth as FdiTeethUpperLeftWithTmp)
+    );
+    const lr = originTeeth.filter(tooth =>
+      FDI_TEETH_LOWER_RIGHT_WITH_TMP.includes(
+        tooth as FdiTeethLowerRightWithTmp
+      )
+    );
+    const ll = originTeeth.filter(tooth =>
+      FDI_TEETH_LOWER_LEFT_WITH_TMP.includes(tooth as FdiTeethLowerLeftWithTmp)
+    );
+
+    const switchedUr = switchTeethWithTmp(ur);
+    const switchedUl = switchTeethWithTmp(ul);
+    const switchedLr = switchTeethWithTmp(lr);
+    const switchedLl = switchTeethWithTmp(ll);
+
+    const newUr = updateTeeth(urTeeth, ur, switchedUr);
+    const newUl = updateTeeth(ulTeeth, ul, switchedUl);
+    const newLr = updateTeeth(lrTeeth, lr, switchedLr);
+    const newLl = updateTeeth(llTeeth, ll, switchedLl);
+    setUrTeeth(newUr);
+    setUlTeeth(newUl);
+    setLrTeeth(newLr);
+    setLlTeeth(newLl);
+  };
 
   return (
     <>
--- src/component/NotationDescription.tsx
+++ src/component/NotationDescription.tsx
@@ -9,8 +9,13 @@ import {
   FDI_PRIMARY_TEETH_LOWER_RIGHT,
   FDI_PRIMARY_TEETH_UPPER_LEFT,
   FDI_PRIMARY_TEETH_UPPER_RIGHT,
+  FdiTeeth,
   FdiTeethWithTmp,
-  NotationType
+  NotationType,
+  convertTeeth,
+  getTeethString,
+  isExistFdiTooth,
+  switchTeethWithTmp
 } from "dental-notation";
 import BabyTeethGrid from "./BabyTeethGrid";
 import TeethGrid from "./TeethGrid";
@@ -34,7 +39,10 @@ export interface TestProps {
   notationType: NotationType;
   isTest?: boolean;
   selectedTeeth?: FdiTeethWithTmp[];
-  onChangeSelectedTeeth?: (teeth: FdiTeethWithTmp[]) => void;
+  onChangeSelectedTeeth?: (
+    teeth: FdiTeethWithTmp[],
+    isConvert?: boolean
+  ) => void;
 }
 
 interface NotationDescriptionProps extends TestProps {
@@ -55,9 +63,68 @@ const NotationDescription = (props: NotationDescriptionProps) => {
     selectedTeeth = [],
     onChangeSelectedTeeth = () => {}
   } = props;
+
+  const existTeeth: FdiTeeth[] = selectedTeeth.filter(isExistFdiTooth);
+
+  const a = getTeethString(
+    convertTeeth(
+      existTeeth,
+      notationType === "fdi"
+        ? "universal"
+        : notationType === "universal"
+        ? "fdi"
+        : "palmer"
+    )
+  );
+
   return (
     <section>
-      <h3 style={MainTitleStyles}>{getNotationName(notationType)} Notation</h3>
+      <div style={{ display: "flex", alignItems: "center", gap: 24 }}>
+        <h3 style={MainTitleStyles}>
+          {getNotationName(notationType)} Notation
+        </h3>
+        {isTest && (
+          <div style={{ display: "flex", alignItems: "center", gap: 12 }}>
+            <p
+              style={{
+                marginBottom: 20,
+                borderRadius: 24,
+                backgroundColor: "lightgray",
+                // color: "#fff",
+                fontWeight: "bold",
+                padding: 8,
+                fontSize: 14,
+                cursor: "pointer"
+              }}
+              onClick={() => {
+                onChangeSelectedTeeth(
+                  switchTeethWithTmp(selectedTeeth) as FdiTeethWithTmp[],
+                  true
+                );
+              }}
+            >
+              Convert Teeth
+            </p>
+            <p
+              style={{
+                marginBottom: 20,
+                borderRadius: 24,
+                backgroundColor: "lightgray",
+                // color: "#fff",
+                fontWeight: "bold",
+                padding: 8,
+                fontSize: 14,
+                cursor: "pointer"
+              }}
+              onClick={() => {
+                onChangeSelectedTeeth([]);
+              }}
+            >
+              Clear
+            </p>
+          </div>
+        )}
+      </div>
       {!isTest && <h6 style={PrimaryTitleStyles}>Primary Teeth</h6>}
       <TeethGrid
         isTest={isTest}
@@ -69,6 +136,73 @@ const NotationDescription = (props: NotationDescriptionProps) => {
         selectedTeeth={selectedTeeth}
         onChangeSelectedTeeth={onChangeSelectedTeeth}
       />
+      {isTest && (
+        <div
+          style={{
+            marginTop: 40,
+            display: "flex",
+            flexDirection: "column",
+            gap: 12
+          }}
+        >
+          <code style={{ display: "flex", flexDirection: "column", gap: 12 }}>
+            {`getTeethString(teeth, "range");`}
+            <p style={{ opacity: 0.7 }}>{getTeethString(existTeeth)}</p>
+          </code>
+          <code style={{ display: "flex", flexDirection: "column", gap: 12 }}>
+            {`getTeethString(teeth, "individual");`}
+            <p style={{ opacity: 0.7 }}>
+              {getTeethString(existTeeth, "individual")}
+            </p>
+          </code>
+          <code style={{ display: "flex", flexDirection: "column", gap: 12 }}>
+            {`convertTeeth(teeth, "${
+              notationType === "fdi"
+                ? "universal"
+                : notationType === "universal"
+                ? "fdi"
+                : "palmer"
+            }");`}
+            <p style={{ opacity: 0.7 }}>
+              {JSON.stringify(
+                convertTeeth(
+                  existTeeth,
+                  notationType === "fdi"
+                    ? "universal"
+                    : notationType === "universal"
+                    ? "fdi"
+                    : "palmer"
+                ),
+                null,
+                2
+              )}
+            </p>
+          </code>
+          <code style={{ display: "flex", flexDirection: "column", gap: 12 }}>
+            {`convertTeeth(teeth, "${
+              notationType === "fdi"
+                ? "palmer"
+                : notationType === "universal"
+                ? "palmer"
+                : "fdi"
+            }");`}
+            <p style={{ opacity: 0.7 }}>
+              {JSON.stringify(
+                convertTeeth(
+                  existTeeth,
+                  notationType === "fdi"
+                    ? "palmer"
+                    : notationType === "universal"
+                    ? "palmer"
+                    : "fdi"
+                ),
+                null,
+                2
+              )}
+            </p>
+          </code>
+        </div>
+      )}
 
       {!isTest && (
         <>
